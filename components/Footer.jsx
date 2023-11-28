94f95703770cc24d102c620e46d5fcf2b8585405
johannDeLaCruz <johann.de.la.cruz2023@gmail.com>
2023-11-28 14:17:07 -0300

    feat(footer): :sparkles: add social media links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,16 +2,18 @@ import Nav from "@components/Nav";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-950 py-8 px-4">
-      <div className="pb-4">
-        <h4 className="font-heading text-h2">
-          <span>Tech</span>
-          <span className="text-primary-500">Hub</span>
-        </h4>
-        <small className="block text-body2">2023 © TechHub</small>
-        <small className="block text-body2">All rights reserved.</small>
+    <footer className="bg-gray-950 py-9">
+      <div className="container">
+        <div className="pb-6">
+          <h4 className="font-heading text-h2">
+            <span>Tech</span>
+            <span className="text-primary-500">Hub</span>
+          </h4>
+          <small className="block text-body2">2023 © TechHub</small>
+          <small className="block text-body2">All rights reserved.</small>
+        </div>
+        <Nav isVisible={true} />
       </div>
-      <Nav isVisible={true} />
     </footer>
   );
 };
---
+++ components/SocialMediaLinks.jsx
@@ -0,0 +1,14 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const SocialMediaLinks = () => {
+  return (
+    <div>
+      <FontAwesomeIcon />
+      <FontAwesomeIcon />
+      <FontAwesomeIcon />
+      <FontAwesomeIcon />
+    </div>
+  );
+};
+
+export default SocialMediaLinks;
