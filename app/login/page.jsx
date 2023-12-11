fa3457a8634880aa9a0334df9791653b0f3267a5
johannDeLaCruz <johann.de.la.cruz2023@gmail.com>
2023-12-11 20:23:57 -0300

    fix(loginPage): :lipstick: fix login page layout

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import InputForm from "@/components/InputForm";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
+import { faGoogle, faGithub } from "@fortawesome/free-brands-svg-icons";
 const page = () => {
   return (
     <div className="container">
@@ -41,16 +42,37 @@ const page = () => {
             id={"password"}
           />
         </div>
-        <button type="submit" className="btn-primary py-2 text-white grow">
+        <button type="submit" className="btn-primary py-2 text-white">
           Login
         </button>
-        <div>
+        <div className="flex justify-between gap-1">
+          <input type="checkbox" name="remember" id="remember" />
           <label htmlFor="remember" className="text-caption">
             Remember Me
           </label>
-          <input type="checkbox" name="remember" id="remember" />
+          <a href="" className="text-caption text-right grow">
+            Forgot your password?
+          </a>
         </div>
       </form>
+      <div className="relative flex py-5 items-center">
+        <div className="flex-grow border-t dark:border-gray-950 border-gray-300"></div>
+        <span className="flex-shrink mx-4 text-caption text-white">OR</span>
+        <div className="flex-grow border-t dark:border-gray-950 border-gray-300"></div>
+      </div>
+      <div className="flex justify-center px-4 py-2 gap-4 ">
+        <button className="btn-gray py-2 px-6 flex gap-2 items-center font-normal">
+          {" "}
+          <FontAwesomeIcon icon={faGoogle} className="text-white" width={20} />
+          <span> Google</span>
+        </button>
+        <button className="btn-gray py-2 px-6 flex gap-2 items-center font-normal">
+          {" "}
+          <FontAwesomeIcon icon={faGithub} className="text-white" width={20} />
+          <span>GitHub</span>
+        </button>
+      </div>
+      <p className="text-caption pt-5 pb-16 text-center">Don&apos;t have an account? Sign up for free</p>
     </div>
   );
 };
