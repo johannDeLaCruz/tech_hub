009803c61cbc5868709e605dc78bb8563197a16f
johannDeLaCruz <johann.de.la.cruz2023@gmail.com>
2023-12-17 11:21:04 -0300

    fix(api): :bug: fix else statement in fetch all items

--- app/api/item/route.js
+++ app/api/item/route.js
@@ -9,6 +9,8 @@ export const GET = async (req) => {
     const items = await User.find({});
     if (items) {
       return new Response(JSON.stringify(items), { status: 200 });
+    } else {
+      return new Response("No items found", { status: 404 });
     }
   } catch (error) {
     console.error("Failed to fetch prompts:", error.message);
