e8a8d3651751fd82e3d4f4728e78bcd5b16678d9
johannDeLaCruz <johann.de.la.cruz2023@gmail.com>
2023-12-24 11:56:04 -0300

    fix(api): :bug: fix duplicate favorites saving bug and setfindbyidandupdate "new" option to "true"

--- app/api/user/[id]/favorites/route.js
+++ app/api/user/[id]/favorites/route.js
@@ -4,19 +4,24 @@ import User from "@models/User";
 export const POST = async (req, { params }) => {
   const { id } = params;
   const { favoriteId } = await req.json();
-
   try {
     await connectToDatabase();
-    const user = await User.findByIdAndUpdate(
+    const user = await User.findById(id);
+    if (!user) {
+      return new Response("No user found", { status: 404 });
+    }
+    const isFavoriteExists = user.favorites.includes(favoriteId);
+    if (isFavoriteExists) {
+      return new Response("Favorite already exists in the user's favorites", {
+        status: 400,
+      });
+    }
+    const updatedUser = await User.findByIdAndUpdate(
       id,
       { $push: { favorites: favoriteId } },
       { new: true }
     );
-    if (!user) {
-      return new Response("No user found", { status: 404 });
-    }
-    await user.save();
-    return new Response(JSON.stringify(user.favorites), { status: 201 });
+    return new Response(JSON.stringify(updatedUser.favorites), { status: 201 });
   } catch (error) {
     console.error(error);
     return new Response("Failed to save favorite", { status: 500 });
@@ -25,11 +30,25 @@ export const POST = async (req, { params }) => {
 
 export const DELETE = async (req, { params }) => {
   const { id } = params;
-  const { favoriteId } = req.json();
+  const { favoriteId } = await req.json();
   try {
     await connectToDatabase();
-    await User.findByIdAndUpdate(id, { $pull: { favorites: favoriteId } });
-    return new Response("Successfully removed favorite!", { status: 200 });
+    const user = await User.findById(id);
+    if (!user) {
+      return new Response("No user found", { status: 404 });
+    }
+    const isFavoriteExists = user.favorites.includes(favoriteId);
+    if (!isFavoriteExists) {
+      return new Response("Favorite not found in the user's favorites", {
+        status: 404,
+      });
+    }
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      { $pull: { favorites: favoriteId } },
+      { new: true }
+    );
+    return new Response(JSON.stringify(updatedUser.favorites), { status: 200 });
   } catch (error) {
     console.error(error);
     return new Response("Failed to remove favorite", { status: 500 });
