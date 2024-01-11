1bbd690e1d811f5206bf4f12c5ea23d94b1d7f1b
johannDeLaCruz <johann.de.la.cruz2023@gmail.com>
2024-01-11 11:19:33 -0300

    fix(card): :recycle: refactor item image

--- components/ItemCard.jsx
+++ components/ItemCard.jsx
@@ -1,8 +1,8 @@
 "use client";
-import ItemImage from "@components/ItemImage";
 import ItemRating from "@components/ItemRating";
 import ItemTags from "@components/ItemTags";
 import Link from "next/link";
+import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState } from "react";
@@ -39,14 +39,21 @@ const ItemCard = ({ item, handleLike, userFavorites, handleFavorite }) => {
   }, [_id, handleFavorite.increment, handleFavorite.itemId]);
 
   return (
-    //alt: bg-gray-950
+    //bg-gray-950
     <article className="rounded-3xl overflow-hidden justify-self-center border dark:border-gray-950 border-gray-200">
-      <ItemImage
-        minimalPrice={minimalPrice}
-        image={image}
-        externalLink={externalLink}
-        _id={_id}
-      />
+      <figure className="relative overflow-hidden w-[20rem] h-[15rem]">
+      <Link href={`/item/${_id}`}>
+        <Image
+          src={`${image}`}
+          alt="item_image"
+          fill={true}
+          className="hover:scale-105 transition-all duration-300 ease-in-out cursor-pointer object-cover"
+        ></Image>
+      </Link>
+      <figcaption className="absolute text-white text-body1 py-2 px-3 bg-gray-950 opacity-50 right-0 top-10 rounded-l-3xl z-5">
+        ${minimalPrice}
+      </figcaption>
+    </figure>
       <div className="flex flex-col gap-1 p-6">
         <div className="flex items-center justify-between gap-3">
           <Link href={`/item/${_id}`} className="text-h3 link-hover">
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 const Nav = ({ navLinks, type, handleNavClick }) => {
-  const footerStyles = "grid grid-cols-2 gap-2 pb-6 grow";
+  const footerStyles = "grid grid-cols-2 gap-y-1 gap-x-16 pb-6 justify-center";
   const headerStyles = "hidden md:flex gap-8";
   const menuStyles = "flex flex-col items-center gap-2 py-8 md:hidden";
 
