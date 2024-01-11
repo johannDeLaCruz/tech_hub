140cf204bf81cb7279f2b019b6a4be94b75c93b5
johannDeLaCruz <johann.de.la.cruz2023@gmail.com>
2024-01-11 12:14:35 -0300

    feat(card): :sparkles: add custom share button

--- components/ItemCard.jsx
+++ components/ItemCard.jsx
@@ -4,7 +4,7 @@ import ItemTags from "@components/ItemTags";
 import Link from "next/link";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+import { faCircleNotch, faShareNodes } from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState } from "react";
 
 const ItemCard = ({ item, handleLike, userFavorites, handleFavorite }) => {
@@ -42,18 +42,30 @@ const ItemCard = ({ item, handleLike, userFavorites, handleFavorite }) => {
     //bg-gray-950
     <article className="rounded-3xl overflow-hidden justify-self-center border dark:border-gray-950 border-gray-200">
       <figure className="relative overflow-hidden w-[20rem] h-[15rem]">
-      <Link href={`/item/${_id}`}>
-        <Image
-          src={`${image}`}
-          alt="item_image"
-          fill={true}
-          className="hover:scale-105 transition-all duration-300 ease-in-out cursor-pointer object-cover"
-        ></Image>
-      </Link>
-      <figcaption className="absolute text-white text-body1 py-2 px-3 bg-gray-950 opacity-50 right-0 top-10 rounded-l-3xl z-5">
-        ${minimalPrice}
-      </figcaption>
-    </figure>
+        <Link href={`/item/${_id}`}>
+          <Image
+            src={`${image}`}
+            alt="item_image"
+            fill={true}
+            className="hover:scale-105 transition-all duration-300 ease-in-out cursor-pointer object-cover"
+          ></Image>
+        </Link>
+        <figcaption className="absolute text-white text-body1 py-2 px-3 bg-gray-950 opacity-50 right-0 top-10 rounded-l-3xl z-5">
+          ${minimalPrice}
+        </figcaption>
+        <button className="absolute border-2 border-primary-500 rounded-full p-2 hover:bg-primary-500 bottom-4 right-4 group duration-300">
+          <svg
+            viewBox="0 0 24 24"
+            className="fill-primary-500 stroke-primary-500 group-hover:fill-white group-hover:stroke-white"
+            style={{             
+              width: "1rem",
+              height: "1rem",
+            }}
+          >
+            <path d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92 1.61 0 2.92-1.31 2.92-2.92s-1.31-2.92-2.92-2.92z"></path>
+          </svg>
+        </button>
+      </figure>
       <div className="flex flex-col gap-1 p-6">
         <div className="flex items-center justify-between gap-3">
           <Link href={`/item/${_id}`} className="text-h3 link-hover">
