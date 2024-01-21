5d2ed0953b1593229eb3f72633f687c76f511def
johannDeLaCruz <johann.de.la.cruz2023@gmail.com>
2024-01-20 21:18:25 -0300

    fix(auth): :bug: fix github auth refresh token

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -51,6 +51,13 @@ const handler = NextAuth({
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
     }),
   ],
   session: {
